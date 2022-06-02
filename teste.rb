8014ec2291710dda7ef3ccad4b01f2e682a1a261
gilson dourado <gilsondourado10@gmail.com>
2022-06-02 08:09:22 -0300

    arquivo teste git flow

--- teste.rb
+++ teste.rb
@@ -1,4 +1,15 @@
-class Nome
-    puts " o meu nome é Gilson"
+
+fred = Class.new do
+  def meth1
+    "hello"
   end
-   puts
+  def meth2
+    "bye"
+  end
+end
+
+puts a = fred.new     #=> #<#<Class:0x100381890>:0x100376b98>
+puts a.meth1          #=> "hello"
+puts a.meth2          #=> "bye"
+
+
